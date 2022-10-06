4f2062dcaf36c37312812710fd7657fa7331863b
anton <okseniukanton@gmail.com>
2022-10-06 11:36:30 +0300

    dev added eureka server(load balancer)

--- fraud/src/main/java/org/example/fraud/FraudAppilication.java
+++ fraud/src/main/java/org/example/fraud/FraudAppilication.java
@@ -2,8 +2,10 @@ package org.example.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudAppilication {
     public static void main(String[] args) {
         SpringApplication.run(FraudAppilication.class, args);
